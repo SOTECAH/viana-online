183daacd7d80c55bdeb5637967aa7388c4d2ebeb
Saul Mayorquin <saulmadi@gmail.com>
2020-04-16 18:42:15 -0600

    Updates

---
+++ precache-manifest.0adc6b11ed2c76246aa4b0b7c2a47f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17594a4c83c0c6d6c3c6c967e894a6a9",
+    "url": "/viana-online/index.html"
+  },
+  {
+    "revision": "f0332ead5d3ea991d1ed",
+    "url": "/viana-online/static/css/main.4f9e7f71.chunk.css"
+  },
+  {
+    "revision": "e665daa1e3eb5303fe08",
+    "url": "/viana-online/static/js/2.b1c38a77.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/viana-online/static/js/2.b1c38a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0332ead5d3ea991d1ed",
+    "url": "/viana-online/static/js/main.779f5358.chunk.js"
+  },
+  {
+    "revision": "64891806d493bb7a07eb",
+    "url": "/viana-online/static/js/runtime-main.91b6bc3b.js"
+  },
+  {
+    "revision": "4f26e29644f9359464accc284ae5ae78",
+    "url": "/viana-online/static/media/aviso.4f26e296.jpg"
+  }
+]);
